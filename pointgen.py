00a14dc466f70565e990c906a41092b85e95cb1f
rmaries <rmaries@gmail.com>
2013-12-10 07:16:34 +0530

    creating faces

--- pointgen.py
+++ pointgen.py
@@ -1,9 +1,7 @@
 import itertools
 def pointgen(x):
-    '''
-    x ---> list of coordinates of diagonal points of the box
+    '''x ---> list of coordinates of diagonal points of the box
     This function will create the coordinates for the edges of the box
-    I assume that box is aligned with xyz plane
-    '''
+    I assume that box is aligned with xyz plane'''
     trans = list(zip(*x))
     return list(itertools.product(trans[0],trans[1],trans[2]))
