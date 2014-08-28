e685c26c9d36d8671474f8545d41a2010a8c1eaa
mesnardo <mesnardo@gwu.edu>
2014-08-28 11:49:40 -0400

    [phugoid script] Add docstrings instead of comments.
    Docstrings replaced comments and define the arguments of the function.
    
    Increased the size of title and labels in the plot.

--- lessons/01_phugoid/phugoid.py
+++ lessons/01_phugoid/phugoid.py
@@ -1,25 +1,45 @@
+# Script to plot the flight path of the phugoid using Lanchester's model.
+# It uses the sign convention and formulae provided by Lanchester (1909).
+
 import numpy
 import matplotlib.pyplot as plt
 
-# use the sign convention and formulae provided by Lanchester (1909)
-
-# calculate the radius of curvature of the flight path at any point
 def radius_of_curvature(y, yt, C):
-#	return 2./(C*(1./y)**1.5 - 2./3./yt)
-        return -1*yt / (1./3 - C/2.*(yt/y)**1.5)
+	"""Returns the radius of curvature of the flight path at any point.
+	
+	Arguments
+	---------
+	y -- current depth below the reference horizontal line.
+	yt -- initial depth below the reference horizontal line.
+	C -- constant of integration.
+	"""
+	#return 2./(C*(1./y)**1.5 - 2./3./yt)
+    return -1*yt / (1./3 - C/2.*(yt/y)**1.5)
 
-# given the coordinates of a point, the center around which it much revolve,
-# and the angle of rotation, calculate the new position of the point
 def rotate(x, y, xCenter, yCenter, angle):
+	"""Returns the new position of the point.
+
+	Arguments
+	---------
+	x, y -- previous position of the point.
+	xCenter, yCenter -- center of rotation.
+	angle -- angle of rotation
+	"""
 	dx = x - xCenter
 	dy = y - yCenter
 	xNew = dx*numpy.cos(angle) - dy*numpy.sin(angle)
 	yNew = dx*numpy.sin(angle) + dy*numpy.cos(angle)
 	return xCenter + xNew, yCenter + yNew
 
-# plot the flight path. The inputs are the trim height and the initial position 
-# and orientation of the aircraft
-def plot_flight_path(yt, y0, theta0):	
+def plot_flight_path(yt, y0, theta0):
+	"""Plots the flight path.
+
+	Arguments
+	---------
+	yt -- trim height of the glider.
+	y0 -- initial height of the glider.
+	theta0 -- initial orientation of the glider.
+	"""
 	# arrays to store the coordinates of the flight path
 	N = 1000
 	y = numpy.zeros(N)
@@ -32,12 +52,12 @@ def plot_flight_path(yt, y0, theta0):
 
 	# calculate the constant C
 	#C = numpy.sqrt(y[0])*(numpy.cos(theta) - y[0]/yt/3.)
-        C = (numpy.cos(theta) - 1./3*y[0]/yt)*(y[0]/yt)**.5
+    C = (numpy.cos(theta) - 1./3*y[0]/yt)*(y[0]/yt)**.5
 
 	# incremental distance along the flight path
-        ds = 1 
+    ds = 1 
         
-        #obtain the curve coordinates
+    #obtain the curve coordinates
 	for i in xrange(1,N):
 		normal = numpy.array([numpy.cos(theta+numpy.pi/2.), numpy.sin(theta+numpy.pi/2.)])
 		R = radius_of_curvature(y[i-1], yt, C)
@@ -48,13 +68,14 @@ def plot_flight_path(yt, y0, theta0):
 
 	# generate a plot
 	plt.figure(figsize=(10,6))
-        plt.plot(x, -y,'k-',label="$z_t=\ %.1f,\\,z_1=\ %.1f,\\,\\theta_1=\ %.2f$" % (yt, y[0], theta0))
-        plt.axis('equal')
-	plt.title("Flight path for $C$ = %.3f" % C)
-        plt.xlabel("$x$", fontsize=16)
-	plt.ylabel("$z$", fontsize=16)
+    plt.plot(x, -y,
+			 color = 'k', ls='-', lw=2.0,
+			 label="$z_t=\ %.1f,\\,z_1=\ %.1f,\\,\\theta_1=\ %.2f$" % (yt, y[0], theta0))
+    plt.axis('equal')
+	plt.title("Flight path for $C$ = %.3f" % C, fontsize=18)
+    plt.xlabel("$x$", fontsize=18)
+	plt.ylabel("$z$", fontsize=18)
 	plt.legend()
-        plt.show()
+    plt.show()
 
-
-
+# End of File
