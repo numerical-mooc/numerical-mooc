2b8a26aae7cdead0e46c07469c040cb5e9133384
Marieswaran <rmaries@gmail.com>
2014-08-30 12:08:02 +0530

    Delete pointgen.py

--- pointgen.py
+++
@@ -1,7 +0,0 @@
-import itertools
-def pointgen(x):
-    '''x ---> list of coordinates of diagonal points of the box
-    This function will create the coordinates for the edges of the box
-    I assume that box is aligned with xyz plane'''
-    trans = list(zip(*x))
-    return list(itertools.product(trans[0],trans[1],trans[2]))
