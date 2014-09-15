59e1a119912ded6cf7d723546329034e9ccf7512
cbell14 <CJ.Bell91@gmail.com>
2014-09-15 14:26:00 -0400

    Varying Velocity and Theta
    Added Velocity and Theta variations to basic code

--- lessons/01_phugoid/My Work/Paper Airplane Model.py
+++ lessons/01_phugoid/My Work/Paper Airplane Model.py
@@ -15,13 +15,11 @@ v_t = 4.9 #trim velocity m/s
 C_D = 1/5.0
 C_L = 1.0 #C_L/C_D = L/D --> 5
 
-#IC's
-#v_0 = np.linspace(v_t,2*v_t,10)
-#theta_0 = np.linspace(0.0,pi/2,10)
-v_0 = 2.0*v_t
-theta_0 = pi/6.0
-x_0 = 0
-y_0 = 6.0 #initial throw height
+#####IC's
+v_0 = np.linspace(v_t,1.5*v_t,10)
+theta_0 = np.linspace(0,pi/4.0,10)
+x_0 = 0.0
+y_0 = 2.3 #initial throw height
 
 #need to solve right hand side of ode's aka f(u) matrix
 
@@ -70,34 +68,45 @@ def euler_step(u, f, dt):
     
     return u + dt * f(u)
     
-#time stepping set-up
 
-T = 100.0   #end time
-dt = 0.1    #time step size
-N = int(T/dt) + 1 #total time steps
-
-#solution array
-u = np.empty((N,4)) #made an empty array N,4 because 4 variables n steps
-u[0] = np.array([v_0, theta_0, x_0, y_0]) #IC's
-
-#time step using euler
-for n in range(N-1): #N-1 because already have initial step
-    if u[n-1,3] != 0:
+def trajectory(v_0,theta_0,x_0,y_0,N):
+    u = np.empty((N,4)) #made an empty array N,4 because 4 variables n steps
+    u[0]= np.array([v_0, theta_0, x_0, y_0]) # IC's
+    for n in range(N-1): #N-1 because already have initial step
         u[n+1] = euler_step(u[n], f, dt)
-    else:
-        n = N
-    
-#plot
-#grab all x and y values
-x = u[:,2]
-y = u[:,3]
+        if u[n+1,3] <= 0:
+            break
+    return u,n
+            
+#time stepping set-up
+T = 100.0   #end time
+dt = 0.05    #time step size
+N = int(T/dt) + 1 #total time steps
+t=np.linspace(0.0,T,N)
 
-plt.figure
-plt.grid(True)
-plt.xlabel(r'x', fontsize=18)
-plt.ylabel(r'y', fontsize=18)
-plt.ylim([0,10])
-plt.xlim([0,40])
-plt.title('Glider Trajectory, flight time = %.2f' % T, fontsize=18)
-plt.plot(x,y, 'k-', lw=2);
-plt.show()
+color=['k-','b-','r-','g-','y-','m-','c-','r-','b-','k-']
+
+for k in range(10):    
+    [u,n] = trajectory(v_0[k],theta_0[5],x_0,y_0,N)
+    x_path= u[0:n,2]
+    y_path= u[0:n,3]
+    plt.figure
+    plt.grid(True)
+    plt.xlabel(r'x', fontsize=18)
+    plt.ylabel(r'y', fontsize=18)
+    plt.title('Varying Velocity Glider Trajectory, flight time = %.2f' % T, fontsize=16)
+    plt.plot(x_path,y_path, color[k], lw=2);
+    plt.show()
+
+plt.figure()
+for k in range(10):    
+    [u,n] = trajectory(v_0[0],theta_0[k],x_0,y_0,N)
+    x_path= u[0:n,2]
+    y_path= u[0:n,3]
+    plt.figure
+    plt.grid(True)
+    plt.xlabel(r'x', fontsize=18)
+    plt.ylabel(r'y', fontsize=18)
+    plt.title('Varying Theta Glider Trajectory, flight time = %.2f' % T, fontsize=16)
+    plt.plot(x_path,y_path, color[k], lw=2);
+    plt.show()
