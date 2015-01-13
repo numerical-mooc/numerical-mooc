db2629e6d65383e4e598447613330b5db8ee0e12
Gil Forsyth <gforsyth@gwu.edu>
2015-01-13 16:24:41 -0500

    remove jacobi function from helper py file

--- lessons/05_relax/laplace_helper.py
+++ lessons/05_relax/laplace_helper.py
@@ -1,4 +1,3 @@
-
 import numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -43,47 +42,6 @@ def plot2D(x, y, p):
     ax.set_ylabel('$y$')
     ax.set_zlabel('$z$')
     ax.view_init(30,45)
-    
-def laplace2d_jacobi(p, y, dx, dy, l2_target):
-    '''Solves the diffusion equation with forward-time, centered scheme
-    
-    Parameters:
-    ----------
-    p: 2D array of float
-        Initial potential distribution
-    y: array of float
-        Nodal coordinates in y
-    dx: float
-        Mesh size
-    dy: float
-        Mesh size
-    l2_target: float
-        Error target
-        
-    Returns:
-    -------
-    p: 2D array of float
-        Potential distribution after relaxation
-
-    iteration_count: int
-        Number of iterations required to meet tolerance
-    '''
-    
-    l2norm = 1
-    pn = numpy.empty_like(p)
-    iteration_count = 0
-
-    while l2norm > l2_target:
-        pn = p.copy()
-        p[1:-1,1:-1] = .25 * (pn[1:-1,2:]+pn[1:-1,:-2]+pn[2:,1:-1]+pn[:-2,1:-1])
-            
-        p[:,-1] = p[:,-2]
-        
-        l2norm = numpy.sum(numpy.abs(p[1:-1,1:-1] - pn[1:-1,1:-1]))
-        
-        iteration_count += 1
-     
-    return p, iteration_count
 
 def p_analytical(x, y):
     '''Returns the analytical solution for the given Laplace Problem on a grid
